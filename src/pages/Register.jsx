8c144d73724a040ee8bc20229b24ca8e253759a7
adaxan <gogy200808@gmail.com>
2024-09-29 23:28:32 +0300

    belgi va bosh harf bolishini ixtiyoriy qildim

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -19,10 +19,8 @@ function Register() {
 
   const validatePassword = (pw) => {
     return (
-      /[A-Z]/.test(pw) &&
       /[a-z]/.test(pw) &&
       /[0-9]/.test(pw) &&
-      /[^A-Za-z0-9]/.test(pw) &&
       pw.length > 4
     );
   };
